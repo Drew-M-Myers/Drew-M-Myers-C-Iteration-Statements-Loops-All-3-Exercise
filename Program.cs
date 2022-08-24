6124089f4ccb2e1de25582fe74eaa6499281fc6a
Drew Myers <drew.myers59@gmail.com>
2022-08-24 17:08:54 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿namespace IterationStatementsandLoopsExercise
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            for (int i = 0; i <= 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+
+
+            /*  static void Main(string[] args)
+            {
+
+                for (int i = 0; i <= 15; i++)
+                {
+                    Console.WriteLine($"{i}");
+                }
+
+            }
+           */
+
+            /*  static void Main(string[] args)
+           {
+
+               for (int i = 0; i <= 15; i++)
+               {
+                   Console.WriteLine($"{i}");
+               }
+
+           }
+          */
+
+            /*  static void Main(string[] args)
+           {
+
+               for (int i = 0; i <= 15; i++)
+               {
+                   Console.WriteLine($"{i}");
+               }
+
+           }
+          */
+
+            /*  static void Main(string[] args)
+           {
+
+               for (int i = 0; i <= 15; i++)
+               {
+                   Console.WriteLine($"{i}");
+               }
+
+           }
+          */
+
+            /*  static void Main(string[] args)
+           {
+
+               for (int i = 0; i <= 15; i++)
+               {
+                   Console.WriteLine($"{i}");
+               }
+
+           }
+          */
+
+            /*  static void Main(string[] args)
+           {
+
+               for (int i = 0; i <= 15; i++)
+               {
+                   Console.WriteLine($"{i}");
+               }
+
+           }
+          */
+
+        }
+
+
+    }
+}
